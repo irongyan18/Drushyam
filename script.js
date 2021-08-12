d536610a4a06380ad3a0c2e2e6071a5ca5095375
Sidharth Kumar <sidharthkumar2k19@gmail.com>
2021-08-13 04:34:58 +0530

    added the feature of lazy loading

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+// code for pull-up-on-scroll navigation bar
+
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   let currentScrollPos = window.pageYOffset;
@@ -8,3 +11,29 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }   
+
+// code to load images in "./resources/gallery"
+
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.srcset = lazyImage.dataset.srcset;
+          lazyImage.classList.remove("lazy");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    });
+
+    lazyImages.forEach(function(lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  } else {
+    // Possibly fall back to event handlers here
+  }
+});
