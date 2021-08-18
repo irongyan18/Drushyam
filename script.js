5a881010fc18113080352550d7befeeb227c0c59
Sid_Arth <53421109+sidharthkumar2019@users.noreply.github.com>
2021-08-18 11:32:15 +0530

    Added pull-up-on-scroll navigation bar, Contact us, About Us and lazy loading. (#12)
    * first commit
    
    * A basic structure
    
    * Some minor changes
    
    * Some minor changes
    
    * Added the gallery section
    
    * Added nav bar
    
    * Update README.md
    
    * Added glow effect on navbar links and smooth scroll effect
    
    * adskfn
    
    * added contact us and about sections
    
    * editted left side bar links
    
    * added the feature of lazy loading
    
    * updated README.md
    
    * Delete readme.txt
    
    Co-authored-by: Pragyan Jaiminy <74226816+irongyan18@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,39 @@
+
+// code for pull-up-on-scroll navigation bar
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}   
+
+// code to load images in "./resources/gallery"
+
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.srcset = lazyImage.dataset.srcset;
+          lazyImage.classList.remove("lazy");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    });
+
+    lazyImages.forEach(function(lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  } else {
+    // Possibly fall back to event handlers here
+  }
+});
